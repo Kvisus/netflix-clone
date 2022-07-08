3b4b5364b5db7ed8878988ee3275456177b83374
Kvisus <eternal.wanderer.z@gmail.com>
2022-07-08 16:10:23 +0300

    type fix

--- typings.d.ts
+++ typings.d.ts
@@ -1,19 +1,19 @@
 export interface Film {
   kinopoiskId: number;
-  imdbId: string;
-  nameRu: string;
-  nameEn: null;
-  nameOriginal: string;
+  imdbId: string | null;
+  nameRu: string | null;
+  nameEn: string | null;
+  nameOriginal: string | null;
   countries: {
     country: string;
   }[];
   genres: {
     genre: string;
   }[];
-  ratingKinopoisk: number;
-  ratingImdb: number;
-  year: number;
+  ratingKinopoisk: number|null;
+  ratingImdb: number | null;
+  year: number|null;
   type: string;
-  posterUrl: string;
-  posterUrlPreview: string;
+  posterUrl: string | StaticImport;
+  posterUrlPreview: string | StaticImport;
 }
